84ed2047097b235b5849232ae2cb0fec7f37a35e
Natan Doyle <96649275+Natan7217@users.noreply.github.com>
2024-02-11 04:48:50 +0300

    commit 5th task

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 PORT = 8080
@@ -7,12 +7,29 @@ PORT = 8080
 @app.route('/')
 @app.route('/index')
 def index():
-    return f"<a href=http://127.0.0.1:{PORT}/promotion_image>Перейти к рекламе</a>"
+    return f"<a href=http://127.0.0.1:{PORT}/promotion_image>Перейти к рекламе</a>" \
+           f"<br><a href=http://127.0.0.1:{PORT}/astronaut_selection>Перейти к анкете</a>"
 
 
 @app.route("/promotion_image")
 def promotion_image():
-    return render_template("base.html", title="title", username=666)
+    return render_template("base.html")
+
+
+@app.route('/astronaut_selection', methods=['POST', 'GET'])
+def form_sample():
+    if request.method == 'GET':
+        return render_template("astro_form.html")
+    elif request.method == 'POST':
+        print(request.form['lname'])
+        print(request.form['fname'])
+        print(request.form['educ'])
+        print(request.form.getlist('profession'))
+        print(request.form['sex'])
+        print(request.form['about'])
+        print(request.form['file'])
+        print(request.form['accept'])
+        return "Форма отправлена"
 
 
 if __name__ == '__main__':
